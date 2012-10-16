21a95684c9a7571695121c6ac947dea2dd112fa2
Sean M. Duncan <sean@devmode.com>
2012-10-16 09:59:45 -0500

    added a spec to verify that enum pseudo-constants are not shared between enum instances

--- spec/mws/enum_spec.rb
+++ spec/mws/enum_spec.rb
@@ -4,7 +4,7 @@ module Mws
 
   describe 'Enum' do
 
-    let (:options) do 
+    let (:options) do
       {
         pending: 'Pending',
         unshipped: [ 'Unshipped', 'PartiallyShipped' ],
@@ -15,39 +15,45 @@ module Mws
       }
     end
 
+    before(:all) do
+      OrderStatus = Enum.for options
+    end
+
     it 'should not allow instance creation via new' do
       expect { Enum.new }.to raise_error NoMethodError
     end
 
-    it 'should construct a constant-like accessor for each provided symbol' do
-      OrderStatus = Enum.for options
+    it 'should construct a pseudo-constant accessor for each provided symbol' do
       options.each do | key, value |
-        OrderStatus.send(key.to_s.upcase.to_sym).should_not be nil?
+        OrderStatus.send(key.to_s.upcase.to_sym).should_not be nil
       end
     end
 
+    it 'should not share pseudo-constants between enumeration instances' do
+      EnumOne = Enum.for( foo: 'Foo', bar: 'Bar', baz: 'Baz' )
+      EnumTwo = Enum.for( bar: 'BAR', baz: 'BAZ', quk: 'QUK' )
+      expect { EnumOne.QUK }.to raise_error NoMethodError
+      expect { EnumTwo.FOO }.to raise_error NoMethodError
+      EnumOne.BAR.should_not == EnumTwo.BAR
+    end
+
     it 'should be able to find an enum entry from a symbol' do
-      OrderStatus = Enum.for options
       OrderStatus.for(:pending).should == OrderStatus.PENDING
     end
 
     it 'should be able to find an enum entry from a string' do
-      OrderStatus = Enum.for options
       OrderStatus.for('Pending').should == OrderStatus.PENDING
     end
 
     it 'should be able to provide a symbol for an entry' do
-      OrderStatus = Enum.for options
       OrderStatus.PENDING.sym.should == :pending
     end
 
     it 'should be able to provide a value for an enum entry' do
-      OrderStatus = Enum.for options
       OrderStatus.PENDING.val.should == 'Pending'
     end
 
     it 'should be able to handle multivalued enum entries' do
-      OrderStatus = Enum.for options
       OrderStatus.for(:unshipped).should == OrderStatus.UNSHIPPED
       OrderStatus.for('Unshipped').should == OrderStatus.UNSHIPPED
       OrderStatus.for('PartiallyShipped').should == OrderStatus.UNSHIPPED
